68d6e7ced14a265de4d790551696906cf2011d7a
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-18 14:51:16 +0100

    Main nav styling

---
+++ services.php
@@ -0,0 +1,11 @@
+<?php include('perch/runtime.php'); ?>
+<?php perch_layout('main-header'); ?>
+<main class="main-content services-main-content" id="main-content">
+<h1>Ahoy</h1>
+	  <?php perch_content('Placeholder'); ?>
+</main>
+<?php perch_layout('universal-footer'); ?>
+<?php perch_layout('universal-footer-end-js'); ?>
+<!-- TODO: Remove debug -->
+<!-- <?php PerchUtil::output_debug(); ?> -->
+
