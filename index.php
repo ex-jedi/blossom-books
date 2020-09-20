e3dab2dca9fb8492a769d3e99e2afd162941603b
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-20 14:43:33 +0100

    Changing homepage Perch templates. Section One

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('main-header'); ?>
 <main class="main-content homepage-main-content" id="main-content">
-  <?php perch_content("Main Content"); ?>
+  <?php perch_content("Section One"); ?>
+  <?php perch_content("Section Two"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <?php perch_layout('main-footer-end-js'); ?>
