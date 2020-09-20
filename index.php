f23cec9e75bcd05bf57a25d115885e389247f7ca
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-20 19:22:37 +0100

    Adding contact forn

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <main class="main-content homepage-main-content" id="main-content">
   <?php perch_content("Section One"); ?>
   <?php perch_content("Section Two"); ?>
+  <?php perch_content("Main Contact Form"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <?php perch_layout('main-footer-end-js'); ?>
