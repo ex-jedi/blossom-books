c7c0851495874f96a156f5ea511a57a329bae80c
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-22 19:52:32 +0100

    About me page content

--- about-me.php
+++ about-me.php
@@ -2,7 +2,7 @@
 <?php perch_layout('main-header'); ?>
 <?php perch_layout("about-me-header-end"); ?>
 <main class="main-content about-me-main-content" id="main-content">
-
+  <?php perch_content("Main Content"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <?php perch_layout('main-footer-end-js'); ?>
