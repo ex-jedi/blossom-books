0e6f122bedce64574a3879a51e66d8354fb2c35f
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-22 19:57:16 +0100

    About me page first pass

--- about-me.php
+++ about-me.php
@@ -3,6 +3,7 @@
 <?php perch_layout("about-me-header-end"); ?>
 <main class="main-content about-me-main-content" id="main-content">
   <?php perch_content("Main Content"); ?>
+  <?php perch_content("Main Contact Form"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <?php perch_layout('main-footer-end-js'); ?>
