e5f437e39e6059619b1e9d97792a3e12ecbfaca4
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-23 18:26:59 +0100

    Formatting date

--- articles/post.php
+++ articles/post.php
@@ -3,8 +3,8 @@
 	<main class="blog-main-content blog-post-main-content main-content">
 		<article class="blog-post-article" itemscope itemtype="https://schema.org/BlogPosting" itemprop="blogPost">
 			<section class="blog-section blog-post-section">
-				<?php perch_blog_post_categories(perch_get('s')); ?>
-								<?php perch_blog_post(perch_get('s')); ?>
+
+				<?php perch_blog_post(perch_get('s')); ?>
 			</section>
 		</article>
 	</main>
