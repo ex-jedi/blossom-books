83335a78b041f3fbf1feffd522a6c3965a3acf28
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-23 13:57:52 +0100

    Removing blog cookie warning

--- articles/post.php
+++ articles/post.php
@@ -1,13 +1,10 @@
 <?php include('../perch/runtime.php'); ?>
 <?php perch_layout('blog-post-header'); ?>
-	<main class="blog-main-content blog-post-main-content">
+	<main class="blog-main-content blog-post-main-content main-content">
 		<article class="blog-post-article" itemscope itemtype="https://schema.org/BlogPosting" itemprop="blogPost">
 			<section class="blog-section blog-post-section">
 				<?php perch_blog_post_categories(perch_get('s')); ?>
 				<?php perch_blog_post(perch_get('s')); ?>
-			</section>
-			<section class="contact-form section">
-				<?php perch_content("General Contact Form"); ?>
 			</section>
 		</article>
 	</main>
