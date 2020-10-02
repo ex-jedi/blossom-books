254a0abd9bb93fc8649eaec68920020d2c862b8b
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-10-02 15:05:15 +0100

    Removing site wrapper div from main header & footer

--- perch/templates/layouts/main-header.php
+++ perch/templates/layouts/main-header.php
@@ -39,13 +39,13 @@
 <?php perch_content("Cookie Warning"); ?>
 
 </head>
-	<body class="<?php perch_page_attribute('bodyClass', array('template' => 'bits.html' )); ?>">
-				<div class="site-wrapper" itemscope itemtype="http://schema.org/LocalBusiness">
-			<header class="main-header">
-				<!-- Skip to main content link  -->
-				<a class="show-on-focus"  href="#main-content">Skip to main content</a>
+	<body class="<?php perch_page_attribute('bodyClass', array('template' => 'bits.html' )); ?> itemscope itemtype="http://schema.org/LocalBusiness"">
 
 					<?php perch_pages_navigation(array(
 							'hide-extensions' => true,
 					)); ?>
+			<header class="main-header">
+				<!-- Skip to main content link  -->
+				<a class="show-on-focus"  href="#main-content">Skip to main content</a>
+
 					<?php perch_content("Main Logo"); ?>
