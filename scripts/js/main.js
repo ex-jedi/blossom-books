dd866ecadb1afa8beeaa5500d548ccf82325b760
Mark <mark@pixelsmiths.co.uk>
2020-10-16 16:27:39 +0100

    Homepage section one color change on scroll

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -11,9 +11,26 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(CSSRulePlugin, ScrollTrigger, ExpoScaleEase, RoughEase, SlowMo);
 
-// ********** GSAP Timelines **********
+// ********** GSAP Scroll Trigger Animations **********
 
-// gsap.to('.main-logo', { duration: 2, x: 500 });
+ScrollTrigger.defaults({
+  markers: true,
+});
+
+ScrollTrigger.create({
+  trigger: '.homepage-section-one',
+  start: 'top center',
+  end: 'bottom center',
+  markers: true,
+  id: 'Homepage Section',
+  toggleClass: 'active',
+  onEnter: () => console.log('Enter'),
+  onLeave: () => console.log('Leave'),
+  onEnterBack: () => console.log('Enter again'),
+  onLeaveBack: () => console.log('Leave again'),
+  onUpdate: (self) => console.log('update', self.progress.toFixed(3)),
+  onToggle: (self) => console.log('toggled', self.isActive),
+});
 
 // *=========================================
 // ** Cookie Warning  **
@@ -57,3 +74,61 @@ function textAreaScrollHandler() {
 }
 
 textAreaInput.addEventListener('scroll', textAreaScrollHandler);
+
+// ********** Scroll Trigger Examples **********
+
+// TODO: Delete ScrollTrigger examples
+
+//* Scrolltrigger in tween example
+// gsap.to('.animation-test', {
+//   scrollTrigger: {
+//     trigger: '.animation-test',
+//     start: 'top 90%',
+//     end: 'top 10%',
+//     scrub: 2,
+//     markers: true,
+//     toggleActions: 'resume pause reverse resume',
+//   },
+//   y: -200,
+// });
+
+// * Scroll Trigger Timeline
+// const tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: '.animation-test',
+//     start: 'top 70%',
+//     end: 'top 30%',
+//     scrub: 2,
+//     markers: true,
+//     toggleActions: 'resume pause reverse resume',
+//   },
+// });
+
+// tl.to('.animation-test', {
+//   y: -200,
+//   rotation: 360,
+//   ease: 'none',
+// });
+
+// * ScrollTrigger defaults example
+ScrollTrigger.defaults({
+  markers: true,
+});
+
+// * ScrollTrigger instance example
+// ScrollTrigger.create({
+//   trigger: '.homepage-section-two',
+//   start: 'top 90%',
+//   end: 'bottom 10%',
+//   // scroller: '#main-content',
+//   // horizontal: true,
+//   markers: true,
+//   id: 'Test',
+//   toggleClass: 'active',
+//   onEnter: () => console.log('Enter'),
+//   onLeave: () => console.log('Leave'),
+//   onEnterBack: () => console.log('Enter again'),
+//   onLeaveBack: () => console.log('Leave again'),
+//   onUpdate: (self) => console.log('update', self.progress.toFixed(3)),
+//   onToggle: (self) => console.log('toggled', self.isActive),
+// });
