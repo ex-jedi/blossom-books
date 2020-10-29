86b6c5c10c3841b41f4ff7ab6ee1c11337b696f0
Mark <mark@pixelsmiths.co.uk>
2020-10-29 17:01:16 +0000

    Cursor for cat subheading and slower transition

--- perch/templates/layouts/blog-footer-end-js.php
+++ perch/templates/layouts/blog-footer-end-js.php
@@ -1,4 +1,5 @@
 <!-- General JS file -->
+<script src="/scripts/minified/main-min.js"></script>
 <script type="text/javascript" src="/scripts/minified/blog-min.js"></script><!-- Local universal JS files -->
 </body>
 </html>
