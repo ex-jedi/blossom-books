3f1cb4fdeb2927d8e013827ef44d4d5ecb2f7166
Mark <mark@pixelsmiths.co.uk>
2020-10-29 16:41:18 +0000

    Changed PHP path method for page file

--- perch/templates/layouts/main-header.php
+++ perch/templates/layouts/main-header.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" href="/css/main.css?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].'/css/main.css');?>">
 	<?php $page_css_file	=  perch_page_attribute('css', array('template' => 'bits.html'), true); ?>
-	<link rel="stylesheet" href="<?php echo $page_css_file ?>?v=<?= filemtime(__DIR__.'../../../../'.$page_css_file);?>">
+	<link rel="stylesheet" href="<?php echo $page_css_file ?>?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].$page_css_file);?>">
 	<!-- Typekit  -->
 	<link rel="stylesheet" href="https://use.typekit.net/ytg3fuz.css">
 	<!-- Perch Meta -->
