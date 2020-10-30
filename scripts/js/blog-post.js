08773d8840994426067b2bf08aaf2334f93adeab
Mark <mark@pixelsmiths.co.uk>
2020-10-30 17:48:06 +0000

    Changing blog paragraph scroll starts

--- scripts/js/blog-post.js
+++ scripts/js/blog-post.js
@@ -11,15 +11,31 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(CSSRulePlugin, ScrollTrigger);
 
-// ********** Blog post paragrapg fade in **********
+// ********** Blog post paragraph fade in **********
 const blogPostParagraphs = document.querySelectorAll('.blog-text-block p');
 
-blogPostParagraphs.forEach((title) => {
+blogPostParagraphs.forEach((paragraph) => {
   ScrollTrigger.create({
-    trigger: title,
+    trigger: paragraph,
     toggleClass: 'paragraph-reveal',
-    start: 'top 90%',
-    end: 'bottom 10%',
-    markers: true,
+    start: 'top 100%',
+    end: 'bottom -400px',
+    id: 'Paragraphs',
+  });
+});
+
+// ********** Blog main image fade in **********
+const blogPostImage = document.querySelectorAll('.blog-post-image');
+
+blogPostImage.forEach((image) => {
+  gsap.to(image, {
+    scrollTrigger: {
+      trigger: image,
+      start: 'top 90%',
+      end: 'bottom 90%',
+      scrub: 1,
+      toggleActions: 'resume',
+    },
+    scale: 1.3,
   });
 });
