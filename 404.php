926bac3c4e57b37c5b9893ad68f65ea44e02c3d4
Mark <mark@pixelsmiths.co.uk>
2020-11-05 13:33:04 +0000

    Error page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php include('perch/runtime.php'); ?>
+<?php perch_layout('main-header'); ?>
+<?php perch_layout("services-header-end"); ?>
+<main class="main-content services-main-content" id="main-content">
+	<?php perch_content("404 Page Content"); ?>
+</main>
+<?php perch_layout('main-footer'); ?>
+<?php perch_layout('error-page-footer-end-js'); ?>
+<!-- TODO: Remove debug -->
+<!-- <?php PerchUtil::output_debug(); ?> -->
+
