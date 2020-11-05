fb23301b94841f22701693ca2fe98efbaa0b6d3f
Mark <mark@pixelsmiths.co.uk>
2020-11-05 16:20:50 +0000

    Setting up 404 image switcher

--- scripts/js/error-page.js
+++ scripts/js/error-page.js
@@ -5,4 +5,26 @@
 // *=========================================
 // ** Picture Switcher  **
 // *=========================================
-console.log('Hello');
+
+const images = document.querySelectorAll('.error-page-main-image');
+
+//* Copied from homepage
+const headerImage = document.querySelectorAll('.header-main-image');
+function headerImageChange() {
+  headerImage.forEach((image) => {
+    // console.log(image);
+    if (image.classList.contains('header-main-image-showing')) {
+      image.classList.remove('header-main-image-showing');
+    } else {
+      image.classList.add('header-main-image-showing');
+    }
+  });
+}
+
+(function loop(min = 1000, max = 6000, randomNumber = Math.random()) {
+  const rand = Math.floor(randomNumber * (max - min) + min);
+  setTimeout(() => {
+    headerImageChange();
+    loop();
+  }, rand);
+})();
