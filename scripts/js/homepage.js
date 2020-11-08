a7b756e05cc6cd34b1ad4025c30892ab6793bbd2
Mark <mark@pixelsmiths.co.uk>
2020-11-08 16:38:41 +0000

    Plaeholder code for plant animation

--- scripts/js/homepage.js
+++ scripts/js/homepage.js
@@ -53,3 +53,41 @@ function headerImageChange() {
     loop();
   }, rand);
 })();
+
+// ********** Plant Animation **********
+
+// const tl = gsap.timeline({
+//   ScrollTrigger: {
+//     trigger: '.header-plant-image-wrapper',
+//     start: 'top center',
+//     end: 'bottom center',
+//     id: 'Homepage Section',
+//     markers: true,
+//   },
+// });
+
+// tl.addLabel('lines')
+//   .addLabel('fill', 3)
+//   .fromTo('.cls-3', { drawSVG: 0 }, { duration: 3, drawSVG: '100%' }, 'start')
+//   .fromTo('.cls-3', { fill: '#fff' }, { duration: 1, fill: '#ebe5e4' }, 'fill')
+//   .fromTo('.cls-4', { drawSVG: 0 }, { duration: 3, drawSVG: '100%' }, 'start')
+//   .fromTo('.cls-4', { fill: '#fff' }, { duration: 1, fill: '#c9bbb7' }, 'fill')
+//   .fromTo('.cls-5', { drawSVG: 0 }, { duration: 3, drawSVG: '100%' }, 'start')
+//   .fromTo('.cls-5', { fill: '#fff' }, { duration: 1, fill: '#c9bbb7' }, 'fill')
+//   .fromTo('.cls-6', { drawSVG: 0 }, { duration: 3, drawSVG: '100%' }, 'start')
+//   .fromTo('.cls-6', { fill: '#fff' }, { duration: 1, fill: '#e1d8d6' }, 'fill')
+//   .fromTo('.cls-7', { drawSVG: 0 }, { duration: 3, drawSVG: '100%' }, 'start')
+//   .fromTo('.cls-7', { fill: '#fff' }, { duration: 1, fill: '#f3f0ef' }, 'fill')
+//   .fromTo('.cls-8', { drawSVG: 0 }, { duration: 3, drawSVG: '100%' }, 'start')
+//   .fromTo('.cls-8', { fill: '#fff' }, { duration: 1, fill: '#ded5d3' }, 'fill')
+//   .fromTo('.cls-9', { drawSVG: 0 }, { duration: 3, drawSVG: '100%' }, 'start')
+//   .fromTo('.cls-9', { fill: '#fff' }, { duration: 1, fill: '#fbfafa' }, 'fill');
+
+// ScrollTrigger.create({
+//   trigger: '.header-plant-image-wrapper',
+//   start: 'top center',
+//   end: 'bottom center',
+//   id: 'Homepage Section',
+//   toggleClass: 'working',
+//   markers: true,
+// });
