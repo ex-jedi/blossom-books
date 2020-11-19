e273ff7c427ae6258c9faf0f2b40915a4171f4df
Mark <mark@pixelsmiths.co.uk>
2020-11-19 15:30:39 +0000

    Lib, utils and handlers files

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -89,26 +89,30 @@ fadeInParagraphs.forEach((paragraph) => {
 // ** Cookie Warning  **
 // *=========================================
 
-const cookieBanner = document.querySelector('.cookie-warning-wrapper');
-const cookieWarningButton = document.querySelector('.cookie-warning-button');
+function cookieWarning() {
+  const cookieBanner = document.querySelector('.cookie-warning-wrapper');
+  const cookieWarningButton = document.querySelector('.cookie-warning-button');
 
-if (localStorage.getItem('cookieSeen') !== 'shown') {
-  cookieBanner.classList.add('show-cookie-warning');
-} else {
-  cookieBanner.style.display = 'none';
+  if (localStorage.getItem('cookieSeen') !== 'shown') {
+    cookieBanner.classList.add('show-cookie-warning');
+  } else {
+    cookieBanner.style.display = 'none';
+  }
+
+  cookieWarningButton.addEventListener(
+    'click',
+    () => {
+      localStorage.setItem('cookieSeen', 'shown');
+      cookieBanner.classList.remove('show-cookie-warning');
+      cookieBanner.addEventListener('transitionend', () => {
+        cookieBanner.style.display = 'none';
+      });
+    },
+    { once: true }
+  );
 }
 
-cookieWarningButton.addEventListener(
-  'click',
-  () => {
-    localStorage.setItem('cookieSeen', 'shown');
-    cookieBanner.classList.remove('show-cookie-warning');
-    cookieBanner.addEventListener('transitionend', () => {
-      cookieBanner.style.display = 'none';
-    });
-  },
-  { once: true }
-);
+cookieWarning();
 
 // *=========================================
 // ** Main  **
