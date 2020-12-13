04302466c64ee87c3afea41edeb4bc5f59a213a2
Mark <mark@pixelsmiths.co.uk>
2020-12-13 16:11:31 +0000

    Better default sharing image templating

--- perch/templates/layouts/main-header.php
+++ perch/templates/layouts/main-header.php
@@ -20,7 +20,7 @@
 	$mainsitename      = "Blossom Books";
 	$pagetitlename = " - Blossom Books";
 	// Needs to be put in images folder
-	$sharing_image = '/images/default-facebook-image.png';
+	$sharing_image = '/images/default-sharing-image.png';
 
 	PerchSystem::set_var('domain',$domain);
 	PerchSystem::set_var('mainurl',$mainurl);
