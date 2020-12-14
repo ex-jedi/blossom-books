3ae65215a57d4d06bbf852cee354f115fac7e783
Mark <mark@pixelsmiths.co.uk>
2020-12-14 20:58:08 +0000

    Schema itemprop="name" in footer

--- perch/templates/layouts/main-footer.php
+++ perch/templates/layouts/main-footer.php
@@ -7,7 +7,7 @@
 			<a href="/terms.php">Terms of use</a>
 		</p>
 		<p class="footer-copyright" >
-			&copy; <?php echo date("Y"); ?> Blossom Books
+			&copy; <?php echo date("Y"); ?> <span itemprop="name">Blossom Books</span>
 		</p>
 		<p class="footer-credit">
 			Web design & development, brand design & illustration by <a href="https://phoenixandphoenix.co/" class="promo-link">phoenix & phoenix</a>
